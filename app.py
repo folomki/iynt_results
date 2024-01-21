8aa2c52d01fa27181e48c7a3ea7f57d0812ae598
folomki <67453145+folomki@users.noreply.github.com>
2024-01-21 11:34:25 +0700

    Стабильные списки команд, жюрей и участников

--- app.py
+++ app.py
@@ -1,103 +1,146 @@
 from flask import Flask, render_template, request, send_file
-import os
+import functions
+
+
 app = Flask(__name__)
 app.secret_key = "anton"
 
+
+# global variable for storing information and the number of teams in SF
+number_of_teams = 3
+number_of_jury = 0
+teams_in_SF = []
+players_in_sf = []
+
+
 @app.route("/")
 def index():
-    delete()
+    functions.delete()
 
     return render_template('index.html')
 
 
 @app.route("/configuration")
 def config():
-    delete()
+    functions.delete()
 
     return render_template('configuration.html')
 
 
 @app.route("/cdownload", methods=['POST', 'GET'])
 def cdownload():
-    number = int(request.form['number']) // 3 + 1
+    functions.delete()
+
+    number_of_teams = int(request.form['number'])
+    number = number_of_teams // 3 + 1 if number_of_teams % 3 != 0 else number_of_teams // 3
     file_name = "configuration.txt"
 
     with open(file_name, 'a') as file:
-        file.write((str(number) + ' ')*4 + '3 ' + '1')
+        file.write((str(number) + ' ') * 4 + '3 ' + '1')
 
     return send_file(file_name, as_attachment=True)
 
 
-@app.route("/intro")
-def intro():
-    delete()
+@app.route("/intro_teams")
+def intro_teams():
+    functions.delete()
 
-    return render_template('intro.html')
+    return render_template("introteams.html")
+
+
+@app.route("/intro", methods=['POST', 'GET'])
+def intro():
+    functions.delete()
+
+    global number_of_teams, number_of_jury
+    number_of_teams = int(request.form['number_of_teams'])
+    number_of_jury = int(request.form['number_of_jury'])
+
+    teams = functions.teamlist("files/teamList.txt")
+    jury = functions.jurylist("files/juryList.txt")
+
+    return render_template('intro.html', teams=teams, number_of_teams=number_of_teams, number_of_jury=number_of_jury, jury=jury)
 
 
 @app.route("/idownload", methods=['POST', 'GET'])
 def idownload():
+    functions.delete()
+
     file_name = "resultsintro.txt"
 
     with open(file_name, 'a') as file:
         file.write('Intro\n')
-        file.write(str(request.form['jury']) + '\n')
 
-        for i in range(1, 34):
-            file.write(str(request.form[('team' + str(i))]) + '\n')
-            file.write(str(request.form[('mark' + str(i))]) + '\n')
+        global number_of_jury, number_of_teams
+
+        for j in range(number_of_jury):
+            file.write(str(functions.jury_index(request.form['jury' + str(j)])) + " ")
+                       
+        file.write('\n')
+
+        for i in range(number_of_teams):
+            file.write(functions.team_index(request.form[('team' + str(i))]) + ' ')
+            file.write(str(request.form[('team_yc' + str(i))]) + '\n')
+            file.write(str(request.form[('team_mark' + str(i))]) + '\n')
 
     return send_file(file_name, as_attachment=True)
 
 
+@app.route("/resultsteams")
+def resultsteams():
+    functions.delete()
 
-@app.route("/results")
+    teams = functions.teamlist("files/teamList.txt")
+
+    return render_template("resultsteams.html", teams=teams)
+
+
+@app.route("/results", methods=['POST', 'GET'])
 def results():
-    delete()
+    functions.delete()
 
-    return render_template('results.html')
+    global number_of_jury, number_of_teams, teams_in_SF, players_in_sf
+    number_of_jury = int(request.form['number_of_jury'])
+    if str(request.form['team2']) == str(request.form['team3']):
+        number_of_teams = 2
+    else:
+        number_of_teams = 3
+
+    teams_in_SF = [request.form['team1'], request.form['team2'], request.form['team3']]
+    players_in_sf = []
+    players_in_sf = [functions.players(teams_in_SF[0]), functions.players(teams_in_SF[1]), functions.players(teams_in_SF[2])]
+
+    jury = functions.jurylist("files/juryList.txt")
+    problems = functions.problem_list("files/problemList.txt")
+
+    return render_template('results.html', number_of_jury=number_of_jury, number_of_teams=number_of_teams, jury=jury, problems=problems, teams_in_SF=teams_in_SF, players_in_sf=players_in_sf)
 
 
 @app.route("/rdownload", methods=['POST', 'GET'])
 def rdownload():
+    functions.delete()
+
     file_name = str(request.form['file_name']) + '.txt'
-    number_teams = int(request.form['number_teams'])
 
     with open(file_name, 'a') as file:
         file.write(str(request.form['sf_name']) + '\n')
-        file.write(str(request.form['teams']) + '\n')
-        file.write(str(request.form['jury']) + '\n')
 
-        file.write(str(request.form['r_problem1']) + '\n')
-        file.write(str(request.form['a_problem1']) + '\n')
-        file.write(str(request.form['participant1']) + '\n')
-        file.write('x x x x x x x\n')
-        file.write(str(request.form['repm1']) + '\n')
-        file.write(str(request.form['oppm1']) + '\n')
-        file.write(str(request.form['revm1']) + '\n')
+        global number_of_jury, number_of_teams, teams_in_SF
+        for i in range(number_of_teams):
+            file.write(str(functions.team_index(teams_in_SF[i])) + ' ')
+        file.write('\n')
 
-        file.write(str(request.form['r_problem2']) + '\n')
-        file.write(str(request.form['a_problem2']) + '\n')
-        file.write(str(request.form['participant2']) + '\n')
-        file.write('x x x x x x x\n')
-        file.write(str(request.form['repm2']) + '\n')
-        file.write(str(request.form['oppm2']) + '\n')
-        file.write(str(request.form['revm2']) + '\n')
+        for j in range(number_of_jury):
+            file.write(str(functions.jury_index(request.form['jury' + str(j)])) + " ")
+        file.write('\n')
 
-        if number_teams == 3:
-            file.write(str(request.form['r_problem3']) + '\n')
-            file.write(str(request.form['a_problem3']) + '\n')
-            file.write(str(request.form['participant3']) + '\n')
+        for i in range(number_of_teams):
+            file.write(str(functions.problem_index(request.form['r_problem0_' + str(i)])) + " " + str(functions.problem_index(request.form['r_problem1_' + str(i)])) + " " + str(functions.problem_index(request.form['r_problem2_' + str(i)])) + '\n')
+            file.write(str(functions.problem_index(request.form['a_problem' + str(i)])) + '\n')
+            file.write(str(functions.players_index(request.form['rep' + str(i)])) + " " + str(functions.players_index(request.form['opp' + str(i)])) + " " + str(functions.players_index(request.form['rew' + str(i)])) + '\n')
             file.write('x x x x x x x\n')
-            file.write(str(request.form['repm3']) + '\n')
-            file.write(str(request.form['oppm3']) + '\n')
-            file.write(str(request.form['revm3']) + '\n')
+            file.write(str(request.form['repp' + str(i)]) + '\n')
+            file.write(str(request.form['oppp' + str(i)]) + '\n')
+            file.write(str(request.form['revp' + str(i)]) + '\n')
 
     return send_file(file_name, as_attachment=True)
-
-
-def delete():
-    for file in os.listdir():
-        if file.endswith(".txt"):
-            file_path = os.path.join(file)
-            os.remove(file_path)
---
+++ functions.py
@@ -0,0 +1,117 @@
+import os
+
+def team_index(team_name):
+    if team_name == '':
+        return ''
+    else:
+        with open('files/teamList.txt', 'r') as file:
+            k = 0
+
+            for line in file:
+                if line.strip() == '':
+                    k += 1
+                if line.strip() == team_name:
+                    return str(k)
+
+
+def teamlist(filename):
+    with open(filename, 'r') as file:
+        stroki = file.readlines()
+        lines = [stroki[0]]
+        for i in range(len(stroki) - 1):
+            if stroki[i].strip() == '':
+                lines.append(stroki[i + 1].strip())
+
+    return lines
+
+
+def players(team):
+    with open("files/teamList.txt", 'r') as file:
+        lines = file.readlines()
+        players = []
+        for i in range(len(lines)):
+            if lines[i].strip() == team:
+                i += 1
+                while lines[i].strip() != "":
+                    if (i + 1) == len(lines):
+                        players.append(lines[i].strip())
+                        i +=1
+                        break
+                    else:
+                        players.append(lines[i].strip())
+                        i += 1
+
+    return players
+
+
+def players_index(name):
+    with open('files/teamList.txt', 'r') as file:
+        k = 0
+        lines = file.readlines()
+        for i in range(len(lines)):
+            if lines[i].strip() == name:
+                i -= 2
+
+                while lines[i].strip() != "":
+                    i -= 1
+                    k += 1
+                return str(k)
+
+
+def jurylist(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+
+    return lines
+
+
+def jury_index(jury_name):
+    with open('files/juryList.txt', 'r') as file:
+
+        k = 0
+        for line in file:
+            if line.strip() == jury_name:
+                return str(k)
+            else:
+                k += 1
+
+
+def problem_list(filename):
+    with open(filename, 'r') as file:
+        stroki = file.readlines()
+        lines = ["None"]
+        for i in range(len(stroki)):
+            if stroki[i].strip() != "fakeProblem":
+                lines.append(stroki[i].strip())
+
+    return lines
+
+
+def problem_index(problem):
+    with open('files/problemList.txt', 'r') as file:
+        k = 0
+        s = ''
+
+        for line in file:
+            if line.strip() == "None":
+                return s
+
+            if line.strip() != problem:
+                k += 1
+            else:
+                return str(k)
+
+
+def delete():
+    for file in os.listdir():
+        if file.endswith(".txt"):
+            file_path = os.path.join(file)
+            os.remove(file_path)
+
+
+def obnylenie(number_of_team, number_of_jury, teams_in_SF, players_in_sf):
+    number_of_teams = 3
+    number_of_jury = 0
+    teams_in_SF = []
+    players_in_sf = []
+    return number_of_team, number_of_jury, teams_in_SF, players_in_sf
